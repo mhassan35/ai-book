9a3002c0340a64216b9c0fd94e3747c6270dd83f
mhassan35 <mhassan909234@gmail.com>
2025-12-05 22:52:36 +0500

    added

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,10 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://github.com/mhassan35',
+  url: 'https://mhassan35.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/aibook',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
